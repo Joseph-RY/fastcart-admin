a404de8f5fa1f254d5bee26640ce37fa68b38329
Joseph-RY <github.joseph.x@gmail.com>
2025-06-02 17:05:57 +0500

    edit src in other-category

--- src/pages/other-categories/other-categories.jsx
+++ src/pages/other-categories/other-categories.jsx
@@ -10,7 +10,7 @@ import DialogContent from "@mui/material/DialogContent";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import { addCategory } from "../../features/add-category/add-category";
-import { editCategory } from "../../features/edit-ctegory/edit-category";
+import { editCategory } from "../../features/edit-category/edit-category";
 import { deleteCategory } from "../../features/delete-category/delete-category";
 import { getCategories } from "../../features/get-categories/get-categories";
 
